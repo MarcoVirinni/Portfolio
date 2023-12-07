9646285ce0479ceed29ddd0fe4fe1d55c34c9232
Marco <marcovirinni@gmail.com>
2023-12-06 21:43:04 -0300

    actualizando maquetado

--- main.js
+++ main.js
@@ -19,32 +19,51 @@ var data = [
     var dasharrayValue = (circumference * skillValue) / 100 + " " + circumference;
     circles[i].setAttribute("stroke-dasharray", dasharrayValue);
   } */
-  $(document).ready(function () {
-    // Función para animar las barras de progreso
-    function animateSkills() {
-        // Selecciona todas las habilidades con la clase 'skill-progress'
-        $('.skill-progress').each(function () {
-            var $bar = $(this).find('path');
-            var $text = $(this).find('text');
-            var progress = $text.text().replace('%', '');
-            
-            // Calcula el porcentaje de la barra de progreso
-            var radius = $bar.attr('r');
-            var circumference = 2 * Math.PI * radius;
-            var strokeDashoffset = circumference * (1 - progress / 100);
-            
-            // Aplica la animación de la barra de progreso
-            $bar.css({
-                'stroke-dasharray': circumference,
-                'stroke-dashoffset': circumference,
-            }).animate({
-                'stroke-dashoffset': strokeDashoffset,
-            }, 1500);
-        });
-    }
+  document.addEventListener('DOMContentLoaded', function () {
+    // Obtener el contenedor del carrusel
+    var carouselContainer = document.getElementById('hard-carousel');
 
-    // Llama a la función para animar las barras de progreso
-    animateSkills();
+    // Lista de habilidades con rutas de imágenes y porcentajes
+    var skills = [
+        { name: 'Habilidad 1', image: 'assets/git.jpg', percentage: 80 },
+        { name: 'Habilidad 2', image: 'assets/css.jpg', percentage: 65 },
+        // ... (Repite el patrón para las habilidades restantes)
+        { name: 'Habilidad 15', image: 'ruta-de-la-imagen-15.jpg', percentage: 90 }
+    ];
+
+    // Generar elementos para cada habilidad
+    skills.forEach(function (skill, index) {
+        var iconElement = document.createElement('i');
+        iconElement.className = 'icono' + (index + 1);
+
+        var imgElement = document.createElement('img');
+        imgElement.setAttribute('src', skill.image);
+        imgElement.setAttribute('alt', skill.name);
+
+        var circleElement = document.createElement('div');
+        circleElement.className = 'percentage-circle';
+
+        var svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svgElement.setAttribute('viewBox', '0 0 100 100');
+
+        var circlePath = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circlePath.setAttribute('cx', '50');
+        circlePath.setAttribute('cy', '50');
+        circlePath.setAttribute('r', '45');
+        circlePath.setAttribute('fill', 'none');
+        circlePath.setAttribute('stroke-linecap', 'round');
+        circlePath.setAttribute('stroke-width', '5');
+        circlePath.setAttribute('stroke', '#fff');
+        circlePath.setAttribute('stroke-dasharray', skill.percentage * 1.256 + ', 125.6');
+        circlePath.setAttribute('transform', 'rotate(-90 50 50)');
+
+        svgElement.appendChild(circlePath);
+        circleElement.appendChild(svgElement);
+
+        iconElement.appendChild(imgElement);
+        iconElement.appendChild(circleElement);
+        carouselContainer.appendChild(iconElement);
+    });
 });
 
 /* ----------------------------loginButton-------------------------- */
